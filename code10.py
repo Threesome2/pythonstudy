eab1c35034d47389e6a75bb4d26d010079b5e359
Threesome <2507461149@qq.com>
2018-08-13 15:42:11 +0800

    add code10.py,code11.py

---
+++ code10.py
@@ -0,0 +1,20 @@
+#code10.py
+# _*_ coding: utf-8 _*_
+
+# n1 = 255
+# n2 = 1000
+# print(hex(n1))
+# print(hex(n2))
+
+
+def my_abs(x):
+	if not isinstance(x,(int,float)):
+		raise TypeError('son of beach')
+
+# my_abs('asd')
+
+n=1
+while n< 1:
+    print(1)
+
+
