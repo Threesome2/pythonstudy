51042410ec8b944f7fb48bf2b01a6bdba4c24c29
Threesome <2507461149@qq.com>
2018-08-13 16:34:49 +0800

    add all delete

--- code2.py
+++
@@ -1,8 +0,0 @@
-print(b'ABC')
-x='ABC'.encode('ascii')
-print(x)
-x='中文'.encode('utf-8')
-print(x)
-#中文不可用ascii
-x=b'ABC'.decode('ascii')
-print(x)
--- code6.py
+++
@@ -1,7 +0,0 @@
-#code6.py
-birth = int(input('birth:\n'))
-if birth < 2000:
-    print('00前')
-else:
-    print('00后')
-    
