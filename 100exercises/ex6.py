2a24b5eda7ec3bea0b1ef131d6677e7293a14dec
nono <2507461149@qq.com>
2018-08-17 00:52:41 +0800

    add 100ex

--- 100exercises/ex6.py
+++ 100exercises/ex6.py
@@ -1,1 +1,11 @@
-ex6.py
+# ex6.py
+
+def fib(n):
+    a=b=1
+    # print(a,b)
+    for i in range(n-1):
+        a,b=b,a+b
+        print(a,b)
+    return a
+
+print(fib(30))
---
+++ 100exercises/ex7.py
@@ -0,0 +1,9 @@
+# ex7.py
+
+def fib(n):
+    if n==1 or n==2:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+
+print(fib(30))
